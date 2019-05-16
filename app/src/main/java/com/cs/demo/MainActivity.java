5b18c6d6da5df9f445add23e7825065620613427
yuqian <634229805@qq.com>
2019-05-16 18:12:28 +0800

    第一次提交

---
+++ app/src/main/java/com/cs/demo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.cs.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, BuildConfig.APP_NAME, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
